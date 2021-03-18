aaea1bff9b1837e8b903fd908ff8721384d4d5d3
Endrey Márk <ungazik@gmail.com>
2021-03-18 10:18:13 +0100

    Minor change: fix automatic routenaming

--- src/Controller/IUserController.php
+++ src/Controller/IUserController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/i/user")
+ * @Route("/iuser")
  */
 class IUserController extends AbstractController
 {
